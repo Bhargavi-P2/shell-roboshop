11971ae8349e8df2669b13e5d6faadd97c0b516e
Bhargavi-P2 <bhanu042p@gmail.com>
2025-08-04 13:18:18 +0530

    shell roboshop

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0cfc48d4773d3b8a6"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" 
@@ -9,6 +8,15 @@ DOMAIN_NAME="bhargavi.xyz"
 
 for instance in ${INSTANCES[@]}
 do
-
-
+    aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0cfc48d4773d3b8a6 
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=test}]" --query "Instances[0].InstanceID --output text
+    if [ $instance != "frontend" ]
+    then 
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+    RECORD_NAME="$instance.$DOMAIN_NAME"
+    else 
+    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    fi 
+    echo "$instance IP address: $IP"    
 done
