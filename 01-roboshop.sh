bf5b5f35d33ffb8dbbbf8028e619403b47bcd6f1
Bhargavi-P2 <bhanu042p@gmail.com>
2026-01-12 18:54:12 +0530

    shell roboshop

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0cfc48d4773d3b8a6"
+SG_ID="sg-0aef5b9d0d33cd435"  
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" 
 "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z01297013K7I993PSBSGS"
@@ -8,7 +8,7 @@ DOMAIN_NAME="bhargavi.xyz"
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0cfc48d4773d3b8a6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text) 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0aef5b9d0d33cd435 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text) 
     if [ $instance != "frontend" ]
     then
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
